10de4c54348364a03c5dc5630f3cea8da316e519
Nikhitha8563 <54335417+Nikhitha8563@users.noreply.github.com>
2020-03-23 17:11:02 -0500

    commit

---
+++ app_api/models/locations.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+/*const openingTimesSchema = new mongoose.Schema({
+  days: {
+    type: String,
+    required: true
+  },
+  opening: String,
+  closing: String,
+  closed: {
+    type: Boolean,
+    required: true
+  }
+});*/
+
+/*const reviewSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  },
+  reviewText: {
+    type: String,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    'default': Date.now
+  }
+});*/
+
+const locationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+  coords: {
+    type: {type: String},
+    coordinates: [Number]
+  },
+    facebookUrl: String,
+    twitterUrl: String,
+    phoneNumber: Number
+  
+});
+locationSchema.index({coords: '2dsphere'});
+
+mongoose.model('Location', locationSchema);
